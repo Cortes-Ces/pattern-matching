9d887b852b88ccba959b9a3adeed2a9ed0b01496
Cortes-Ces <Cortesces90@gmail.com>
2025-12-02 21:52:43 -0500

    Added a hash pattern

--- main.rb
+++ main.rb
@@ -8,9 +8,13 @@ def interpret(cmd)
   # Array pattern with variables
   in [:sum, a, b] then a + b
 
+  # Hash pattern
+  in { action: :greet, name: name } then "Hello, #{name}!"
+
   else puts 'Unknown command'
   end
 end
 
 puts interpret([:ping])
 puts interpret([:sum, 4, 5])
+puts interpret({ action: :greet, name: 'Cesar' })
